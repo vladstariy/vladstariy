873eea16a0bf164cae426c2100fa05cc4220da2b
vladstariy <vladislav.starowojtov2017@yandex.by>
2017-09-21 23:43:09 +0300

    а

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
-            //gjerigkef
+            //приём
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
